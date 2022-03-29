32b3df0a98349aebafd13cf1813cdf0ba0397887
Yura-Si4kar <si4kar2017@gmail.com>
2022-03-29 13:05:00 +0300

    js

--- scripts.js
+++ scripts.js
@@ -1,19 +1,23 @@
 let action = prompt('Hello! What do you want to do (/,+,*,-)?');
 let numberOne = +prompt('Write the first number.');
 let numberTwo = +prompt('Write the second number.');
+const resultPlus = numberOne + numberTwo;
+const resultMinus = numberOne - numberTwo;
+const resultDivide = numberOne / numberTwo;
+const resultMultiply = numberOne * numberTwo;
 
 switch (action) {
     case '+':
-        alert (numberOne + numberTwo);
+        alert(`result ${numberOne} + ${numberTwo} = ${resultPlus}`);
         break;
     case '-':
-        alert (numberOne - numberTwo);
+        alert(`result ${numberOne} - ${numberTwo} = ${resultMinus}`);
         break;
     case '/':
-        alert (numberOne / numberTwo);
+        alert(`result ${numberOne} / ${numberTwo} = ${resultDivide}`);
         break;
     case '*':
-        alert(numberOne * numberTwo);
+        alert(`result ${numberOne} * ${numberTwo} = ${resultMultiply}`);
         break;
      default:
     alert('Wrong Action');
